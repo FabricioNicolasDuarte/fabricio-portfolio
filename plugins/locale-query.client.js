00cf521e00a8fd6eae45b839affe0d41a70bb6d0
Fabricio Nicolas Duarte <fabricioduarteoficial@gmail.com>
2026-08-29 20:51:13 -0300

    Remove the old query-lang plugin so it stops fighting path locales and freezing the tab.

--- plugins/locale-query.client.js
+++
@@ -1,9 +0,0 @@
-export default defineNuxtPlugin(() => {
-  const { locale } = useLocale()
-  const router = useRouter()
-  router.afterEach((to) => {
-    if (!import.meta.client) return
-    if (to.query.lang === locale.value) return
-    router.replace({ path: to.path, hash: to.hash, query: { ...to.query, lang: locale.value } })
-  })
-})
